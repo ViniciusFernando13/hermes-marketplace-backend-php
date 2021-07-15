987351b3b3eed244b86e9dd70cbe89bdaa9d70ef
ViniciusFernando13 <vihh.fernando@gmail.com>
2021-07-14 21:56:38 -0300

    first comit
    Signed-off-by: ViniciusFernando13 <vihh.fernando@gmail.com>

---
+++ database/migrations/2021_07_15_001553_create_sellers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSellersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sellers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('display_name');
+            $table->string('document')->unique();
+            $table->string('phone');
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sellers');
+    }
+}
